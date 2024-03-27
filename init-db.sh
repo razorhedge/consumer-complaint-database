ab3e6961f7ed2102efe6f92006cf8b594ae22cf4
Matt Romo <mromofer@gmail.com>
2024-03-27 16:50:05 -0300

    Updated bash script

--- init-db.sh
+++ init-db.sh
@@ -9,7 +9,7 @@ done
 
 # Create Table
 psql -v ON_ERROR_STOP=1 --username = "$POSTGRES_USER" --dbname ="$POSTGRES_DB" <<-EOSQL
-    CREATE TABLE IF NOT EXISTS     CREATE TABLE IF NOT EXISTS complaints (
+    CREATE TABLE IF NOT EXISTS complaints (
         date_received text,
         product text,
         sub_product text,
